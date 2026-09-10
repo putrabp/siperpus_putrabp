7c671915743cc796730a0d7feb3128b930af7fc5
putrabp <putrabintangp20@gmail.com>
2026-09-10 17:21:45 +0700

    Add files via upload

---
+++ siperpus_ci_pbp.sql
@@ -0,0 +1,115 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Sep 10, 2026 at 12:18 PM
+-- Server version: 10.4.32-MariaDB
+-- PHP Version: 8.2.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `siperpus_ci_pbp`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `books`
+--
+
+CREATE TABLE `books` (
+  `id` bigint(20) UNSIGNED NOT NULL,
+  `book_code` varchar(20) NOT NULL,
+  `isbn` varchar(20) DEFAULT NULL,
+  `title` varchar(200) NOT NULL,
+  `author` varchar(150) NOT NULL,
+  `publisher` varchar(150) DEFAULT NULL,
+  `publication_year` int(4) UNSIGNED DEFAULT NULL,
+  `category` varchar(100) NOT NULL,
+  `stock_total` int(10) UNSIGNED NOT NULL DEFAULT 0,
+  `stock_available` int(10) UNSIGNED NOT NULL DEFAULT 0,
+  `cover_filename` varchar(255) DEFAULT NULL,
+  `created_at` datetime DEFAULT NULL,
+  `updated_at` datetime DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `books`
+--
+
+INSERT INTO `books` (`id`, `book_code`, `isbn`, `title`, `author`, `publisher`, `publication_year`, `category`, `stock_total`, `stock_available`, `cover_filename`, `created_at`, `updated_at`) VALUES
+(1, 'BK001', '9786020000001', 'Pemrograman PHP Dasar', 'Andi Saputra', 'Informatika', 2025, 'Pemrograman', 10, 10, NULL, '2026-09-10 10:07:57', '2026-09-10 10:07:57'),
+(2, 'BK002', '9786020000002', 'Dasar Database MySQL', 'Budi Santoso', 'Tekno Media', 2024, 'Database', 8, 8, NULL, '2026-09-10 10:07:57', '2026-09-10 10:07:57'),
+(3, 'BK003', '9786020000003', 'Membangun Web dengan CodeIgniter 4', 'Citra Lestari', 'Digital Press', 2026, 'Framework', 5, 5, NULL, '2026-09-10 10:07:57', '2026-09-10 10:07:57'),
+(4, 'BK004', '9786020000004', 'Cara jago Maen ep\" ala KENTUNG', '@putrabelok', 'Instagram', 1945, 'Framework', 666, 6, NULL, '2026-09-10 10:07:57', '2026-09-10 10:07:57'),
+(5, 'BK005', '9786020000004', 'Cara Pus Reng ML ke immo ala KENTUNG', '@putrabelok', 'Instagram', 2222, 'Framework', 2222, 2, NULL, '2026-09-10 10:07:57', '2026-09-10 10:07:57');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `migrations`
+--
+
+CREATE TABLE `migrations` (
+  `id` bigint(20) UNSIGNED NOT NULL,
+  `version` varchar(255) NOT NULL,
+  `class` varchar(255) NOT NULL,
+  `group` varchar(255) NOT NULL,
+  `namespace` varchar(255) NOT NULL,
+  `time` int(11) NOT NULL,
+  `batch` int(11) UNSIGNED NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `migrations`
+--
+
+INSERT INTO `migrations` (`id`, `version`, `class`, `group`, `namespace`, `time`, `batch`) VALUES
+(5, '2026-09-10-085931', 'App\\Database\\Migrations\\CreateBooksTable', 'default', 'App', 1789034837, 1);
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `books`
+--
+ALTER TABLE `books`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `migrations`
+--
+ALTER TABLE `migrations`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `books`
+--
+ALTER TABLE `books`
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+
+--
+-- AUTO_INCREMENT for table `migrations`
+--
+ALTER TABLE `migrations`
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
